53ed65e79085467e968684acda5f2b561e1f139f
Damien Gonot <damien.gonot@gmail.com>
2022-12-30 08:33:15 -0500

    last price

--- src/IbkrTerm.jl
+++ src/IbkrTerm.jl
@@ -27,6 +27,11 @@ function handle(cmd)
             leverage = fetch_leverage(account["id"])
             println(account["accountAlias"] * " (" * account["id"] * ") " * "Leverage: " * leverage["value"])
         end
+    elseif startswith(cmd, "p")
+        (_, symbol) = split(cmd, " ")
+        conid = fetch_conid(symbol)
+        md_snapshot = fetch_md_snapshot(conid)
+        println(md_snapshot)
     elseif cmd ∈ ["exit", "quit"]
         stop()
     else
@@ -54,6 +59,20 @@ function fetch_leverage(accountId)
     s = String(r.body)
     j = JSON.parse(s)
     return j["leverage-s"]
+end
+
+function fetch_conid(symbol)
+    r = HTTP.request("GET", url * "/trsrv/stocks?symbols=" * symbol, require_ssl_verification=false)
+    s = String(r.body)
+    j = JSON.parse(s)
+    return j[symbol][1]["contracts"][1]["conid"]
+end
+
+function fetch_md_snapshot(conid)
+    r = HTTP.request("GET", url * "/md/snapshot?conids=" * string(conid) * "&fields=31", require_ssl_verification=false)
+    s = String(r.body)
+    j = JSON.parse(s)
+    return j[1]["31"]
 end
 
 function stop()
